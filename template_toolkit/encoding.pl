e5bd1427e27ca98eba615ba87aefc490d33baa2f
june.kim <june.kim@kakaocorp.com>
2018-10-22 14:25:57 +0900

    perl - template toolkit encoding 문제

---
+++ template_toolkit/encoding.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+
+binmode STDOUT, ":utf8";
+
+use Template;
+
+use FindBin;
+
+my $tt = Template->new({
+    ENCODING => 'utf8',
+    INCLUDE_PATH => ["$FindBin::Bin/templates",],
+}) || die "$Template::ERROR\n";
+
+my $vars = (
+);
+
+$tt->process("main.html", $vars) || die $tt->error(), "\n";
