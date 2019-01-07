a5a4ae532fc061f1940047d75b80703893e8b83a
june.kim <june.kim@kakaocorp.com>
2019-01-07 22:42:28 +0900

    lookup mx records

---
+++ dns/mx_lookup.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+use Net::DNS;
+
+use Data::Dumper;
+
+my $res = Net::DNS::Resolver->new;
+
+my $domain = "daum.net";
+
+# mx 메서드 이용 방법
+my @mxs = mx($res, $domain);
+# print Dumper(\@mx);
+
+for my $mx (@mxs) {
+    print $mx->preference, ' ', $mx->exchange, "\n";
+}
+
+# query 메서드 이용 방법
+my $reply = $res->query($domain, 'MX');
+
+for my $mx ($reply->answer) {
+    print $mx->preference, ' ', $mx->exchange, "\n";
+}
