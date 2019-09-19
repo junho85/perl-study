89484771cc7d7f985dca5c2659c2da47ab38a74e
june.kim <june.kim@kakaocorp.com>
2019-09-19 16:05:41 +0900

    flat array

---
+++ array/flat.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+my $array_of_array_ref = [[1,2,3],[4,5,6]];
+print(Dumper($array_of_array_ref));
+
+my @result = map {@$_} @$array_of_array_ref;
+
+print(Dumper(\@result));
