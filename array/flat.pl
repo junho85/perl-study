cee3bc81c4446abe5facc3acdbbb9ac1f9baeb07
june.kim <june.kim@kakaocorp.com>
2019-09-19 16:13:58 +0900

    flat array

--- array/flat.pl
+++ array/flat.pl
@@ -5,8 +5,7 @@ use warnings;
 use Data::Dumper;
 
 my $array_of_array_ref = [[1,2,3],[4,5,6]];
-print(Dumper($array_of_array_ref));
+print(Dumper($array_of_array_ref)); # [[1,2,3],[4,5,6]]
 
 my @result = map {@$_} @$array_of_array_ref;
-
-print(Dumper(\@result));
+print(Dumper(\@result)); # [1,2,3,4,5,6]
